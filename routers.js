9276b3d9655b48bd528410efaf03a397e3a33b98
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-02-17 02:43:06 -0300

    Refatoração utilizando Node.js e Nunjucks

---
+++ routers.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const routes = express.Router();
+
+//principal
+routes.get("/", function (req, res) {
+  return res.render("index");
+});
+
+routes.get("/about", function (req, res) {
+  return res.render("about");
+});
+
+routes.get("/recipes", function (req, res) {
+  return res.render("recipes");
+});
+
+module.exports = routes;
